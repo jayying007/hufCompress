c50523b2522b5c3104da77b9ccf140d5f1f32608
jayying007 <1172510964@qq.com>
2021-07-15 00:56:59 +0800

    Initial Commit

---
+++ hufCompress/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hufCompress
+//
+//  Created by jieying zhuang on 2021/7/15.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
